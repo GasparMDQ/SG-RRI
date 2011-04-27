0d36c57846bbc83c78054bf68561e7da45eb0b4a
Gaspar Zaragoza <gasparmdq@gmail.com>
2011-04-27 00:38:01 -0300

    Minor fix
    Signed-off-by: Gaspar Zaragoza <gasparmdq@gmail.com>

--- apps/informes/modules/home/actions/actions.class.php
+++ apps/informes/modules/home/actions/actions.class.php
@@ -17,6 +17,6 @@ class homeActions extends sfActions
   */
   public function executeIndex(sfWebRequest $request)
   {
-    $this->forward('default', 'module');
+/*    $this->forward('default', 'module');*/
   }
 }
---
+++ apps/informes/modules/home/templates/indexSuccess.php
@@ -0,0 +1,5 @@
+Pagina principal del proyecto <br />
+menu hacia los informes y listados <br />
+cuadro de login <br />
+listado de proximos eventos <br />
+cumpleanios de usuarios del dia o de la semana <br />
